582d515bed7c71f47f401084257d86cb2a175a24
mariosilva22007457 <a22007457@alunos.ulht.pt>
2022-05-17 13:15:54 +0100

    lab 9

--- lab8/portfolio/models.py
+++ lab8/portfolio/models.py
@@ -6,8 +6,8 @@ class Post(models.Model):
     data = models.DateTimeField(auto_now_add=True)
     titulo = models.CharField(max_length=200)
     descricao = models.CharField(max_length=500)
-    link = models.URLField(max_length=200 , blank=True)
-    image = models.ImageField(upload_to='media/' , blank=True)
+    link = models.URLField(max_length=200, blank=True)
+    image = models.ImageField(upload_to='media/', blank=True)
 
 
 
